7c00d9536cf5f52648d9b6db8e46bbc8e40991b9
Mititelu Cristina <mititelucristina29@gmail.com>
2025-07-19 22:26:40 +0300

    recipe-book-server

---
+++ routes/recipes.mjs
@@ -0,0 +1,65 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// Get a list of 50 recipes
+router.get("/", async (req, res) => {
+  let collection = await db.collection("recipes");
+  let results = await collection.find({})
+    .toArray();
+  res.send(results).status(200);
+});
+
+// Fetches the latest 3 recipes
+router.get("/latest", async (req, res) => {
+  let collection = await db.collection("recipes");
+  let results = await collection.aggregate([
+    {"$sort": {"date": -1}},
+    {"$limit": 3}
+  ]).toArray();
+  res.send(results).status(200);
+});
+
+// Get a single recipe
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("recipes");
+  let query = {_id: ObjectId(req.params.id)};
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// Add a new recipe to the collection
+router.post("/", async (req, res) => {
+  let collection = await db.collection("recipes");
+  let newDocument = req.body;
+  newDocument.date = new Date();
+  let result = await collection.insertOne(newDocument);
+  res.send(result).status(204);
+});
+
+// Update the recipe
+router.patch("/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+  const updates = { $set: req.body };
+
+  let collection = await db.collection("recipes");
+  let result = await collection.updateOne(query, updates);
+
+  res.send(result).status(200);
+});
+
+// Delete an entry
+router.delete("/:id", async (req, res) => {
+  const query = { _id: ObjectId(req.params.id) };
+
+  const collection = db.collection("recipes");
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
+});
+
+export default router;
